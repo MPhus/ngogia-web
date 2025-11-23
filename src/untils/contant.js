178be707d7a9d2e81ab2b60c6d82e9ce0d56a7a5
MPhus <phu3009@gmail.com>
2025-11-23 23:53:21 +0700

    v2

--- src/untils/contant.js
+++ src/untils/contant.js
@@ -11,7 +11,7 @@ if (process.env.BUILD_MODE === 'dev') {
 	apiRoot = 'http://localhost:3000'
 }
 if (process.env.BUILD_MODE === 'production') {
-	apiRoot = ''
+	apiRoot = 'https://ngogia-api.onrender.com'
 }
 export const API_CITY = 'https://raw.githubusercontent.com/kenzouno1/DiaGioiHanhChinhVN/master/data.json'
 
